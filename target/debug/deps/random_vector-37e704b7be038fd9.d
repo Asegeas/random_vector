12ef2730f2da21efba7566a7a996e36ce79eb4bd
Asegeas <egeland01@allegheny.edu>
2023-11-09 09:17:45 -0500

    binary

--- target/debug/deps/random_vector-37e704b7be038fd9.d
+++ target/debug/deps/random_vector-37e704b7be038fd9.d
@@ -1,5 +1,5 @@
-/home/egeland01/rust-testing/projects/random_vector/target/debug/deps/random_vector-37e704b7be038fd9: src/main.rs
+/home/egeland01/random_vector/target/debug/deps/random_vector-37e704b7be038fd9: src/main.rs
 
-/home/egeland01/rust-testing/projects/random_vector/target/debug/deps/random_vector-37e704b7be038fd9.d: src/main.rs
+/home/egeland01/random_vector/target/debug/deps/random_vector-37e704b7be038fd9.d: src/main.rs
 
 src/main.rs:
--- target/debug/random_vector.d
+++ target/debug/random_vector.d
@@ -1,1 +1,1 @@
-/home/egeland01/rust-testing/projects/random_vector/target/debug/random_vector: /home/egeland01/rust-testing/projects/random_vector/src/main.rs
+/home/egeland01/random_vector/target/debug/random_vector: /home/egeland01/random_vector/src/main.rs
