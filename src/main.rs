3b470a9464cb499d66cb637c8cdb529b1a043375
Asegeas <egeland01@allegheny.edu>
2023-11-09 09:21:18 -0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use rand::prelude::SliceRandom;
 
-
-
 fn main() {
     // env::set_var("RUST_BACKTRACE", "full");
     let mut selection = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut final_list = vec![];
-    let mut count:i32 = 0;
+    let mut count: i32 = 0;
     let length = selection.len().try_into().unwrap();
     while count < length {
         let index = selection.choose(&mut rand::thread_rng()).unwrap();
